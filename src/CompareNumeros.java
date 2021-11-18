35d698b77e8252b4d22c0c06dd25524c3ca058a6
Lukas-jb <92315994+Lukas-jb@users.noreply.github.com>
2021-11-18 12:15:13 -0500

    Primer commit - clase compararnumero

---
+++ src/CompareNumeros.java
@@ -0,0 +1,12 @@
+public class CompareNumeros {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        System.out.println("\nSe Realizara la comparacion de los siguentes numeros: "+num1+" y "+num2);
+        System.out.println("\nEn este caso");
+        if (num1 == num2) System.out.println("los dos numeros son iguales");
+        if (num1<num2) System.out.println(num1+" es menor que "+num2);
+        if (num1>num2) System.out.println(num1+" es mayor que "+num2);
+    }
+}
