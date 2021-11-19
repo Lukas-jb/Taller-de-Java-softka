05383d00cca91c711873bfbe2e4e0f319079a827
Lukas-jb <92315994+Lukas-jb@users.noreply.github.com>
2021-11-19 00:21:37 -0500

    Se modifica Factura

--- src/Factura/Factura.java
+++ src/Factura/Factura.java
@@ -1,9 +1,37 @@
 package Factura;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+
 public class Factura {
 
     public static void main(String[] args) {
-    double  total = 0;
+        double total = 0;
+        double precio = 150200.50;
+        final double iva= 21;
+
+
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        DecimalFormat df = new DecimalFormat("###.####");
+
+
+        try {
+            System.out.println("ingrese el precio del producto");
+            precio = Double.parseDouble(bf.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        total= (precio*(1+iva/100));
+
+        System.out.println("El precio final total es: ");
+
+        System.out.println(df.format(total));
+
 
     }
 }
